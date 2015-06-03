b8e6eff130cc331b087f6a03d8f36ba2efcc952c
Joren <Jorendb@live.com>
2015-06-03 16:51:07 +0200

    Functional single octave piano

--- app/src/main/java/nl/mprog/pianoapp/MainActivity.java
+++ app/src/main/java/nl/mprog/pianoapp/MainActivity.java
@@ -13,7 +13,7 @@ import android.view.View;
 public class MainActivity extends ActionBarActivity {
 
     private SoundPool soundPool;
-    private int soundID;
+    private int c3, cSharp3, d3, dSharp3, e3, f3, fSharp3, g3, gSharp3, a3, aSharp3, b3, c4;
     boolean loaded = false;
 
     @Override
@@ -30,28 +30,31 @@ public class MainActivity extends ActionBarActivity {
                 loaded = true;
             }
         });
-        soundID = soundPool.load(this, R.raw.singleguitar, 1);
 
+        loadSounds();
+
+    }
+
+    public void loadSounds()
+    {
+        c3 = soundPool.load(this, R.raw.guitarc3, 1);
+        cSharp3 = soundPool.load(this, R.raw.guitarcsharp3, 1);
+        d3 = soundPool.load(this, R.raw.guitard3, 1);
+        dSharp3 = soundPool.load(this, R.raw.guitardsharp3, 1);
+        e3 = soundPool.load(this, R.raw.guitare3, 1);
+        f3 = soundPool.load(this, R.raw.guitarf3, 1);
+        fSharp3 = soundPool.load(this, R.raw.guitarfsharp3, 1);
+        g3 = soundPool.load(this, R.raw.guitarg3, 1);
+        gSharp3 = soundPool.load(this, R.raw.guitargsharp3, 1);
+        a3 = soundPool.load(this, R.raw.guitara3, 1);
+        aSharp3 = soundPool.load(this, R.raw.guitarasharp3, 1);
+        b3 = soundPool.load(this, R.raw.guitarb3, 1);
+        c4 = soundPool.load(this, R.raw.guitarc4, 1);
     }
 
 
     public void buttonOnClick(View view)
     {
-        /*switch(view.getId())
-        {
-            case R.id.button1:
-                // Code for button 1 click
-                break;
-
-            case R.id.button2:
-                // Code for button 2 click
-                break;
-
-            case R.id.button3:
-                // Code for button 3 click
-                break;
-        }*/
-
         AudioManager audioManager = (AudioManager) getSystemService(AUDIO_SERVICE);
         float actualVolume = (float) audioManager
                 .getStreamVolume(AudioManager.STREAM_MUSIC);
@@ -61,8 +64,73 @@ public class MainActivity extends ActionBarActivity {
 
         if (loaded)
         {
-            soundPool.play(soundID, volume, volume, 1, 0, 1f);
-            Log.e("Test", "Played sound");
+            switch(view.getId())
+            {
+                case R.id.buttonC3:
+                    soundPool.play(c3, volume, volume, 1, 0, 1f);
+                    Log.e("Test", "Played sound");
+                    break;
+
+                case R.id.buttonCSharp3:
+                    soundPool.play(cSharp3, volume, volume, 1, 0, 1f);
+                    Log.e("Test", "Played sound");
+                    break;
+
+                case R.id.buttonD3:
+                    soundPool.play(d3, volume, volume, 1, 0, 1f);
+                    Log.e("Test", "Played sound");
+                    break;
+
+                case R.id.buttonDSharp3:
+                    soundPool.play(dSharp3, volume, volume, 1, 0, 1f);
+                    Log.e("Test", "Played sound");
+                    break;
+
+                case R.id.buttonE3:
+                    soundPool.play(e3, volume, volume, 1, 0, 1f);
+                    Log.e("Test", "Played sound");
+                    break;
+
+                case R.id.buttonF3:
+                    soundPool.play(f3, volume, volume, 1, 0, 1f);
+                    Log.e("Test", "Played sound");
+                    break;
+
+                case R.id.buttonFSharp3:
+                    soundPool.play(fSharp3, volume, volume, 1, 0, 1f);
+                    Log.e("Test", "Played sound");
+                    break;
+
+                case R.id.buttonG3:
+                    soundPool.play(g3, volume, volume, 1, 0, 1f);
+                    Log.e("Test", "Played sound");
+                    break;
+
+                case R.id.buttonGSharp3:
+                    soundPool.play(gSharp3, volume, volume, 1, 0, 1f);
+                    Log.e("Test", "Played sound");
+                    break;
+
+                case R.id.buttonA3:
+                    soundPool.play(a3, volume, volume, 1, 0, 1f);
+                    Log.e("Test", "Played sound");
+                    break;
+
+                case R.id.buttonASharp3:
+                    soundPool.play(aSharp3, volume, volume, 1, 0, 1f);
+                    Log.e("Test", "Played sound");
+                    break;
+
+                case R.id.buttonB3:
+                    soundPool.play(b3, volume, volume, 1, 0, 1f);
+                    Log.e("Test", "Played sound");
+                    break;
+
+                case R.id.buttonC4:
+                    soundPool.play(c4, volume, volume, 1, 0, 1f);
+                    Log.e("Test", "Played sound");
+                    break;
+            }
         }
 
     }
